882a3c81ec1f010356d6b8797adadbec67e7bd87
Mohammed Lajam <eng.mohammedlajam@gmail.com>
2023-01-20 22:16:05 +0100

    adding variables to be used in the project

--- constants.py
+++ constants.py
@@ -1,8 +1,15 @@
-# Constants:
+# This file contains all the variables, which will be used in this repository:
 
+# 1. Signal-preprocessing variables:
 SAMPLING_RATE = 1000
 LOW_FC = 100
 HIGH_FC = 10
 FILTER_ORDER = 8
+
+# 2. Signal Segmentation variables:
+PERIOD = 5
+
+# 3. Feature Extraction variables:
 FRAME_SIZE = 512
 HOP_SIZE = 64
+SPLIT_FREQUENCY = 256
