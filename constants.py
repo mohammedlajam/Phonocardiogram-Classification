60a374cd46782d0c016c2b12e8a06bde4fc60464
Mohammed Lajam <eng.mohammedlajam@gmail.com>
2023-02-20 08:16:57 +0100

    adding variables N_IMFS

--- constants.py
+++ constants.py
@@ -1,12 +1,13 @@
 # This file contains all the variables, which are used in this repository:
 # 1. Paths:
-REPO_PATH = '/Users/mohammedlajam/Documents/GitHub/pcg-classification'
-DATASET_PATH = '/data/pcg_signals/PhysioNet_2016'
-SIG_PRE_PATH = '/data/denoised_signals'
-FEATURE_EXTRACTION_PATH = '/data/extracted_features'
+REPO_PATH = '/Users/mohammedlajam/Documents/GitHub/Phonocardiogram-Classification'
+DATASET_PATH = f'{REPO_PATH}/data/pcg_signals/PhysioNet_2016'
+SIG_PRE_PATH = f'{REPO_PATH}/data/denoised_signals'
+FEATURE_EXTRACTION_PATH = f'{REPO_PATH}/data/extracted_features'
 
 # 2. Signal-preprocessing variables:
 SAMPLING_RATE = 1000
+N_IMF = 1
 LOW_FC = 100
 HIGH_FC = 10
 FILTER_ORDER = 8
@@ -21,4 +22,3 @@ SPLIT_FREQUENCY = 256
 N_MELS = 128
 N_MFCCS = 13
 N_SCALES = 30
-
