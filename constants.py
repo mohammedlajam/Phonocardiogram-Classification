b495fd722f50abd7938c33b5541115bc8645052c
Mohammed Lajam <eng.mohammedlajam@gmail.com>
2023-03-17 10:59:51 +0100

    adding constants for data preparation

--- constants.py
+++ constants.py
@@ -1,9 +1,10 @@
 # This file contains all the variables, which are used in this repository:
 # 1. Paths:
-REPO_PATH = '/Users/mohammedlajam/Documents/GitHub/Phonocardiogram-Classification'
+REPO_PATH = '/Users/mohammedlajam/Documents/GitHub/pcg-classification'
 DATASET_PATH = f'{REPO_PATH}/data/pcg_signals/PhysioNet_2016'
 SIG_PRE_PATH = f'{REPO_PATH}/data/denoised_signals'
 FEATURE_EXTRACTION_PATH = f'{REPO_PATH}/data/extracted_features'
+CROSS_VALIDATION_PATH = f'{REPO_PATH}/data/cross_validation'
 
 # 2. Signal-preprocessing variables:
 SAMPLING_RATE = 1000
@@ -22,3 +23,9 @@ SPLIT_FREQUENCY = 256
 N_MELS = 128
 N_MFCCS = 13
 N_SCALES = 30
+
+# 5. Data Preparation:
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
+N_FOLDS = 6
+CORR_THRESHOLD = 0.8
