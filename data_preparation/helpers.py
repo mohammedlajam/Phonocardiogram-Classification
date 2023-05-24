d6f212eed9e4a9c28ea3dace8090580fdd4ef5ef
Mohammed Lajam <eng.mohammedlajam@gmail.com>
2023-05-24 06:12:30 +0200

    adding functions for computer vision-data preparation

--- data_preparation/helpers.py
+++ data_preparation/helpers.py
@@ -12,12 +12,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+import cv2
+import random
 # EDA Libraries:
 from scipy.stats import probplot
 # Feature Engineering Libraries:
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import StratifiedKFold, StratifiedGroupKFold
+from sklearn.model_selection import StratifiedGroupKFold, GroupShuffleSplit, StratifiedShuffleSplit
 from scipy import stats
 from imblearn.over_sampling import SMOTE
 # Feature Selection Libraries:
@@ -26,7 +29,6 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2, mutual_info_classif
 from sklearn.ensemble import ExtraTreesClassifier
 from scipy.stats import pointbiserialr
-from scipy.stats import pearsonr
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -35,9 +37,9 @@ warnings.filterwarnings('ignore')
 pd.pandas.set_option('display.max_columns', None)
 
 
-# 1. Exploratory Data Analysis (EDA):
-# 1.1. Continuous Numeric Variables:
-class ExploratoryDataAnalysis:
+# 1. Tabular Data:
+# 1.1 Exploratory Data Analysis (EDA):
+class ExploratoryDataAnalysisTabular:
     def __init__(self, dataset):
         self.dataset = dataset
 
@@ -91,11 +93,9 @@ class ExploratoryDataAnalysis:
         sns.heatmap(corr, cmap='RdBu', annot=True, fmt=".2f")
         return plt.show()
 
-# 1.2. Images:
 
-
-# 2. Feature Engineering:
-class FeatureEngineering:
+# 1.2. Feature Engineering:
+class FeatureEngineeringTabular:
     def __init__(self, dataset):
         self.dataset = dataset
 
@@ -158,7 +158,7 @@ class FeatureEngineering:
         return pd.DataFrame(transformed_feature)
 
     @staticmethod
-    def normalize_data(self, x_train, x_test):
+    def normalize_data(x_train, x_test):
         """Function to normalize datasets."""
         scaler = MinMaxScaler()
         x_train_normalized = pd.DataFrame(scaler.fit_transform(x_train), columns=x_train.columns)
@@ -166,7 +166,7 @@ class FeatureEngineering:
         return x_train_normalized, x_test_normalized
 
     @staticmethod
-    def balance_dataset(self, x_train, y_train, rand_state: int):
+    def balance_dataset(x_train, y_train, rand_state: int):
         """Function to balance dataset using SMOTE technique."""
         smote = SMOTE(random_state=rand_state)
         x_train_resampled, y_train_resampled = smote.fit_resample(x_train, y_train)
@@ -185,7 +185,8 @@ class FeatureEngineering:
         return self.dataset
 
 
-class FeatureSelection:
+# 1.3. Feature Selection:
+class FeatureSelectionTabular:
     def __init__(self, x_train, x_test, y_train, y_test):
         self.x_train = x_train
         self.x_test = x_test
@@ -273,3 +274,130 @@ class FeatureSelection:
         else:
             return correlated_features
 
+
+# 2. Computer Vision:
+# 2.1. Exploratory Data Analysis (EDA):
+class ExploratoryDataAnalysisCV:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def load_images_references(directory):
+        references = pd.read_csv(directory)
+        return references
+
+    @staticmethod
+    def extract_initial_analysis(references):
+        """Function to get initial insight about the references."""
+        # checking number of subjects and features:
+        print(f'Number of Subjects: {references.shape[0]}')
+        # Checking how many classes:
+        print(f'Classes: {references["class"].unique()}')
+        # Checking the number of subject per class:
+        print(f'Number of subjects per class:\n {references["class"].value_counts()}')
+
+    @staticmethod
+    def get_random_image_size(directory):
+        # Get a list of all image file names in the directory
+        image_files = [file for file in os.listdir(directory) if file.endswith(('.jpg', '.jpeg', '.png'))]
+
+        # Select a random image file
+        random_image_file = random.choice(image_files)
+
+        # Return the path to the random image
+        random_image_path = os.path.join(directory, random_image_file)
+
+        # Load the image
+        image = cv2.imread(random_image_path)
+
+        # Get the size of the image
+        height, width, channels = image.shape
+
+        return height, width, channels
+
+
+# 2.2. Feature Engineering:
+class FeatureEngineeringCV:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def resize_images(images, new_width: int, new_height: int):
+        """Function to resize the images. It returns a list of the resized images."""
+        resized_images = []
+        for image in images:
+            resized_image = cv2.resize(image, (new_width, new_height))
+            resized_images.append(resized_image)
+        return resized_images
+
+    @staticmethod
+    def split_images(images, labels, references, test_size, rand_state):
+        """Function to split images into train and test set based on Group Stratified splitting."""
+        group_splitter = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=rand_state)
+
+        # Convert references to groups
+        groups = references['signal_id'].tolist()
+
+        x_train, x_test, y_train, y_test = None, None, None, None  # Initialize the variables
+        for train_index, test_index in group_splitter.split(images, labels, groups):
+            x_train = [images[i] for i in train_index]
+            x_test = [images[i] for i in test_index]
+            y_train = [labels[i] for i in train_index]
+            y_test = [labels[i] for i in test_index]
+            break
+        # Converting x_train and x_test into Numpy arrays:
+        x_train = np.array(x_train, dtype=np.float32)
+        x_test = np.array(x_test, dtype=np.float32)
+        return x_train, x_test, y_train, y_test
+
+    @staticmethod
+    def create_cross_validation(images, labels, references, n_folds: int, rand_state: int):
+        """Function to generate the indices for train and test set of each fold of the cross
+        validation. It returns two list containing the indices for train and test sets."""
+        cv = StratifiedGroupKFold(n_splits=n_folds, shuffle=True, random_state=rand_state)
+        train_indices = []
+        test_indices = []
+
+        # Convert references to groups
+        groups = references['signal_id'].tolist()
+        for fold, (train_idx, test_idx) in enumerate(cv.split(images,
+                                                              labels,
+                                                              groups=groups)):
+            train_indices.append(train_idx)
+            test_indices.append(test_idx)
+        return train_indices, test_indices
+
+    @staticmethod
+    def create_lists_from_indices(images: list, labels: list, indices_list: list, index: int):
+        """Function to return image_fold and label_fold based on the indices generated from cross-validation."""
+        fold_indices = indices_list[index]
+        image_fold = [images[i] for i in fold_indices]
+        label_fold = [labels[i] for i in fold_indices]
+
+        # Converting into Numpy Array:
+        image_fold = np.array(image_fold, dtype=np.float32)
+        label_fold = np.array(label_fold, dtype=np.float32)
+        return image_fold, label_fold
+
+    @staticmethod
+    def balance_images(images, labels, rand_state: int):
+        """Function to balance images using SMOTE technique."""
+        reshaped_images = images.reshape(images.shape[0], -1)
+        smote = SMOTE(random_state=rand_state)
+
+        images_resampled, labels_resampled = smote.fit_resample(reshaped_images, labels)
+        # Reshape the resampled data back to the original shape
+        images_resampled = images_resampled.reshape((-1, 128, 128))
+        return images_resampled, labels_resampled
+
+    @staticmethod
+    def normalize_images(images):
+        """Function to normalize the images. It returns a list of the normalized images."""
+        normalized_images = []
+        for image in images:
+            normalized_image = image / 255.0
+            normalized_images.append(normalized_image)
+
+        # Converting x_train and x_test into Numpy arrays:
+        normalized_images = np.array(normalized_images, dtype=np.float32)
+        return normalized_images
